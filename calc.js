404dfb0383c5a123bfc9ac543a4adff79a4159fc
Matthew Halliday <matthalliday54@hotmail.com>
2023-06-30 20:08:33 +0100

    Create calc container and row divs

--- calc.js
+++ calc.js
@@ -15,6 +15,23 @@ function divide(x,y) {
     return x / y;
 }
 
-let numLeft = 0;
-let numRight = 0;
+let num1 = 0;
+let num2 = 0;
 let operator = "add";
+
+function operate(num1, num2, operator){
+    switch (operator) {
+        case "add":
+            add(num1, num2);
+            break;
+        case "subtract":
+            subtract(num1, num2);
+            break;
+        case "multiply":
+            multiply(num1, num2);
+            break;
+        case "divide":
+            divide(num1, num2);
+            break;
+    }
+}
