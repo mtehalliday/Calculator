ec2bb023e6257f741bb6001635a06ca6dc2c288e
Matthew Halliday <matthalliday54@hotmail.com>
2023-06-30 18:47:53 +0100

    Create js functions, and operator function

---
+++ calc.js
@@ -0,0 +1,20 @@
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+let numLeft = 0;
+let numRight = 0;
+let operator = "add";
