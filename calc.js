e6c33b1935d541c4f33b93ba7ea03eaa3baaf537
Matthew Halliday <matthalliday54@hotmail.com>
2023-07-02 14:20:54 +0100

    Create display function

--- calc.js
+++ calc.js
@@ -1,4 +1,6 @@
-const numbers = document.querySelectorAll("div.num");
+const numbers = document.querySelectorAll("button.num");
+const test = document.querySelector("#equals");
+const display = document.querySelector(".display");
 
 
 
@@ -41,10 +43,27 @@ function operate(num1, num2, operator){
 
 let input = "";
 
-console.log(numbers)
 
-for (let i = 0; i < numbers.length; i++){
-    numbers[i].addEventListener("click", function(e) {
-        console.log(e);
-    })
+function displayInput(input) {
+    display.textContent = input;
 }
+
+
+numbers.forEach((num) => {
+    num.addEventListener("click", function(e) {
+        input += e.target.value;
+        displayInput(input);
+    });
+});
+
+console.log(input);
+
+
+
+//for (let i = 0; i < numbers.length; i++){
+//    numbers[i].addEventListener("click", function(e) {
+//        console.log(e);
+//    })
+//}
+
+test.addEventListener("click", () => console.log("fixed"));
